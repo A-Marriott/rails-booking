431a1169593707a8ec95aecec02d1bf49684b0ad
Alex Marriott <marriottaog@gmail.com>
2021-04-29 14:54:04 +0100

    backend routes + controller working for room CRUD actions

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,6 +1,44 @@
 class Api::V1::RoomsController < Api::V1::BaseController
+  before_action :set_room, only: [:show, :update, :destroy]
+
   def index
     @rooms = Room.all
     render json: @rooms
+  end
+
+  def show
+    render json: @room
+  end
+
+  def create
+    @room = Room.create!(room_params)
+    if @room
+      render json: @room
+    else
+      render json: @room.errors
+    end
+  end
+
+  def update
+    if @room.update(room_params)
+      render json: @room
+    else
+      render json: @room.errors
+    end
+  end
+
+  def destroy
+    @room.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:base_price, :name, :headline, :description, :capacity, :published, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :rooms, only: [ :index ]
+      resources :rooms, only: [ :index, :create, :show, :update, :destroy ]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
